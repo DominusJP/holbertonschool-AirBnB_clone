cacf4b774acf7186c5ad69588c4cb46c5831fb46
MarcosBremermann <mabm2004@gmail.com>
2023-10-25 14:06:51 +0000

    Task 3 tests

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -12,3 +12,13 @@ print(my_model_json)
 print("JSON of my_model:")
 for key in my_model_json.keys():
     print("\t{}: ({}) - {}".format(key, type(my_model_json[key]), my_model_json[key]))
+
+def test_unique_objects(self):
+    obj1 = BaseModel()
+    obj2 = BaseModel()
+    self.assertNotEqual(obj1.to_dict(), obj2.to_dict())
+
+def test_str_repr(self):
+    obj1 = BaseModel()
+    str_obj1 = f"[{type(obj1).__name__}] ({obj1.id}) {obj1.__dict__}"
+    self.assertEqual(str(obj1), str_obj1)
