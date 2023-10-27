9908c5f03a9f560532235cd593c2a3aad04ab14f
DominusJP <6775@holbertonstudents.com>
2023-10-27 07:03:44 -0700

    task 8

---
+++ models/user.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+class User that inherits from BaseModel:
+"""
+from models.base_model import BaseModel
+
+
+class User(BaseModel):
+    """User class that inherits from BaseModel"""
+
+    def __init__(self, *args, **kwargs):
+        """Constructor for the class"""
+        super().__init__(*args, **kwargs)
+        self.email = ""
+        self.password = ""
+        self.first_name = ""
+        self.last_name = ""
